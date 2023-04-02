8f74c8a28ad5901b9ff0e7264128867bfc83f206
Amar Dugonja <amadex@amadex.com>
2023-04-02 22:23:11 +0200

    Update iss.sh

--- iss.sh
+++ iss.sh
@@ -43,7 +43,7 @@ echo -e "${GREEN}4. Do you want to add swap space? (yes/no)${NC}"
 read answer
 if [[ $answer == "yes" ]]; then
     if ! swapon --show | grep -q "swap"; then
-        echo -e "${WHITE}Enter the swap size in GB:${NC}"
+        echo -e "${WHITE}Enter the swap size in GB (just number without GB part):${NC}"
         read swap_size
         fallocate -l "${swap_size}G" /swapfile
         chmod 600 /swapfile
